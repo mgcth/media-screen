56b2b4619ae574f2b6e600d0ae91dc013467e868
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-10-21 22:21:32 +0200

    Update build system

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-import platform
-
-base = (["requests==2.27.1", "spotipy==2.19.0", "Pillow==9.0.1", "pylast==4.5.0"],)
-
-setup(name="media_screen", install_requires=base)
