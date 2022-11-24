ed5841a4ae2425393c3e96083a655fe8437ba166
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-11-24 23:28:08 +0100

    Update unti test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -2,10 +2,9 @@ from unittest.mock import patch
 from media_screen.main import init
 
 
+@patch("media_screen.main.__name__", "__main__")
 @patch("media_screen.main.main")
 def test_init(mock_main):
-    """
-    Dummy test.
-    """
+    """Dummy test."""
     init()
     mock_main.assert_called_once()
