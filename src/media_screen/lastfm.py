8dd172a2c42f2f402de2bc9db15d50fea2bae06c
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-11-24 22:53:07 +0100

    Update lastfm to have a similar interface to spotify

--- src/media_screen/lastfm.py
+++ src/media_screen/lastfm.py
@@ -1,59 +1,116 @@
+"""Last.fm module."""
 import time
-import pylast
-from media_screen.misc import config, KILO, MEGA
+import requests
+from PIL import Image
+from io import BytesIO
+from media_screen.misc import CONFIG, KILO, MEGA
+from pylast import LastFMNetwork, User, PyLastError, MalformedResponseError
 
 
-config = config["last.fm"]
+config = CONFIG["last.fm"]
 
 
 class LastFM:
-    """
-    LastFM class.
-    """
+    """LastFM class."""
 
     def __init__(self):
-        """
-        Initialise the class.
-        """
-
-        self._count = None
+        """Initialise the Last.fm class."""
+        self._clear()
 
         try:
             username = config["username"]
-            self.network = pylast.LastFMNetwork(
+            network = LastFMNetwork(
                 api_key=config["api_key"],
                 api_secret=config["api_secret"],
                 username=username,
             )
-            self.user = pylast.User(username, self.network)
-
-        except pylast.PyLastError as e:
-            print("Problems connecting to last.fm")
-            print(e)
-            self.network = None
-
-    def get_currently_playing(self):
-        """
-        Get the currently playing track
-        """
-
-        time.sleep(5)  # for now, should try to id last.fm and spotify
-
-        try:
-            track = self.user.get_now_playing()
-            if track != None:
-                self._count = track.get_userplaycount()
-        except pylast.MalformedResponseError as e:
-            print("Last.fm get track error")
-            print(e)
+            network.enable_rate_limit()
+            self.user = User(username, network)
+        except PyLastError:
+            raise PyLastError("Failed to connect to last.fm")
 
     @property
-    def count(self):
-        """
-        Get item_ok property.
+    def currently_playing(self):
+        """Get the currently playing track
 
-        Output:
-            count: times track has been played
+        Returns:
+            playing track
         """
+        try:
+            self._track = self.user.get_now_playing()
+        except MalformedResponseError:
+            raise MalformedResponseError("Last.fm get track error.")
 
+        if self._track != None:
+            self._set_track_details()
+        else:
+            self._clear()
+
+        return self._track
+
+    @property
+    def new_track(self):
+        """Test if new track is playing.
+
+        Returns:
+            true for new track, false for not
+        """
+        if self.user.get_now_playing() == self._track:
+            return False
+
+        return True
+
+    @property
+    def count(self) -> int:
+        """Get track count.
+
+        Returns:
+            times track has been played
+        """
         return self._count
+
+    @property
+    def item_ok(self) -> bool:
+        """Check if item is ok to draw."""
+        return self._item_ok
+
+    @property
+    def track(self) -> str:
+        """Get track title."""
+        return self._track_title
+
+    @property
+    def album(self) -> str:
+        """Get album title."""
+        return self._album_title
+
+    @property
+    def artists(self) -> str:
+        """Get artists name."""
+        return self._artists_name
+
+    def _get_track_image(self) -> None:
+        """Download the track album cover image and hold it in memory."""
+        if self._item_ok:
+            response = requests.get(self._image_url)
+            self.image = Image.open(BytesIO(response.content))
+
+    def _set_track_details(self) -> None:
+        """Store track details in object."""
+        self._item_ok = True
+        self._count = self._track.get_userplaycount()
+        self._track_title = self._track.title
+        self._album_title = self._track.get_album().title
+        self._artists_name = self._track.get_artist().name
+        self._image_url = self._track.get_cover_image()
+
+        self._get_track_image()
+
+    def _clear(self) -> None:
+        """Clear object details."""
+        self._item_ok = False
+        self._count = None
+        self._track_title = None
+        self._album_title = None
+        self._artists_name = None
+        self._image_url = None
