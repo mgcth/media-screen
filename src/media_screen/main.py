8113843db74af011b0a6d2a4481a77748b670ba7
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-11-24 22:53:18 +0100

    Simplify main for now

--- src/media_screen/main.py
+++ src/media_screen/main.py
@@ -1,20 +1,35 @@
-import sys
 import time
+from media_screen.lastfm import LastFM
+from media_screen.screen import Screen
 
 
 def main():
     """
-    Main function.
+    Main function defining the run loop.
     """
+    delay = 2
+    velocity = 0
 
-    pass
+    lastfm = LastFM()
+    lastfm.get_currently_playing()
+
+    with Screen() as screen:
+        screen.draw(0, lastfm, 0, delay)
+
+        while True:
+            if lastfm.new_track:
+                continue
+
+            lastfm.currently_playing
+            screen.draw(0, lastfm, 0, delay)
+
+            time.sleep(0.5)
 
 
 def init():
     """
     Entry point to main.
     """
-
     if __name__ == "__main__":
         main()
 
