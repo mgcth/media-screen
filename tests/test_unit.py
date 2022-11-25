fe43aa89301de954e8e88688b7546749038a6043
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-11-25 16:58:26 +0100

    Assign track to lastm object

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -1,6 +1,9 @@
 from unittest.mock import patch
+from media_screen.main import init
 
 
+@patch("media_screen.main.__name__", "__main__")
+@patch("media_screen.main.main")
 def test_init(mock_main):
     """Dummy test."""
     pass
