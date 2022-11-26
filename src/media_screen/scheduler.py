77dd43736bd27b8471e52a98331c764678ce091e
Mladen Gibanica <11275336+mgcth@users.noreply.github.com>
2022-11-26 14:36:48 +0100

    Scheduler improvments

--- src/media_screen/scheduler.py
+++ src/media_screen/scheduler.py
@@ -1,33 +1,62 @@
 """Scheduler module."""
 import time
+import datetime
+from typing import Callable
 from media_screen.lastfm import LastFM, Song
 
 from media_screen.screen import Screen
 from media_screen.misc import KILO, MEGA
 
-DELAY = 30
+DELAY = 60
+NIGHT_START = datetime.time(0, 0, 0)
+NIGHT_END = datetime.time(8, 0, 0)
+DAY_START = datetime.time(8, 0, 0)
+DAY_END = datetime.time(23, 59, 59)
+
+
+def get_delay():
+    """Get delay based on play stats and time of day."""
+    time_now = datetime.datetime.now().time()
+
+    if NIGHT_START <= time_now <= NIGHT_END:
+        delay = DELAY * 10
+
+    if DAY_START <= time_now <= DAY_END:
+        delay = DELAY * 1
+
+    delay = int(delay * KILO)  # s to ms
+
+    return delay
 
 
 class Item:
     """Item class."""
 
-    def __init__(self, song: Song, progress: int = 0, delay: int = DELAY) -> None:
+    def __init__(
+        self,
+        song: Song,
+        duration: int = 0,
+        progress: int = 0,
+        set_delay: Callable = get_delay,
+    ) -> None:
         """Item initialisation.
 
         Args:
             song: song
-            duration: duration of track
+            duration: duration of previous track
             progress: last.fm API does not provide this, set to 0
-            delay: delay in API call
+            set_delay: delay function
         """
-        delay = delay * KILO  # s to ms
+        delay = set_delay()
         self._song = song
         self._duration = song.duration if song.duration > delay else delay
         self._progress = progress
         self._delay = delay
         self._creation_time = self._get_time()
 
-        self._track_end_time = self._creation_time + self._duration
+        duration = self._duration if duration == 0 else duration
+
+        self._track_end_time = self._creation_time + duration
         self._time_delay = self._creation_time + self._delay
 
     @property
@@ -43,8 +72,7 @@ class Item:
     @property
     def delay_timer(self):
         """Delay timer."""
-        delay = self._time_delay - self._get_time()
-        return delay
+        return self._time_delay - self._get_time()
 
     def reset_delay(self):
         """Reset delay timer."""
@@ -72,16 +100,14 @@ class Scheduler:
         self.delay = delay
         self.velocity = velocity
         self.lastfm = LastFM()
-        self.item = None
+        self.item = Item(self.lastfm.song)
 
     def run(self) -> None:
         """Scheduler run method."""
+        new_track = self._set_track()
+
         with Screen() as screen:
             while True:
-                if self.item is None:
-                    new_track = self._set_track()
-                    continue
-
                 if self.item.timer < 0 or self.item.delay_timer < 0:
                     new_track = self._set_track()
 
@@ -89,7 +115,7 @@ class Scheduler:
                     screen.draw(0, self.item.song, 0, self.item._delay)
                     new_track = False
 
-                time.sleep(DELAY)
+                time.sleep(2)
 
     def _set_track(self) -> bool:
         """Get the current track's progress and duration and set track end time.
@@ -98,12 +124,7 @@ class Scheduler:
             new track (True), same track (False)
         """
         new_track = self.lastfm.currently_playing
+        duration = 0 if new_track else self.item.timer
+        self.item = Item(self.lastfm.song, duration)
 
-        if new_track is False:
-            if self.item:
-                self.item.reset_delay()
-            return new_track
-
-        if self.lastfm.item_ok:
-            self.item = Item(self.lastfm.song)
-            return new_track
+        return new_track
